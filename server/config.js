235bea04ec2023d1517d081380bdc4666d31b5e5
jvila <julio.vila@raymondjames.com>
2018-01-06 09:19:06 -0500

    Added websocket client connection

--- server/config.js
+++ server/config.js
@@ -1,3 +1,5 @@
 const port = 1111;
+const WS_IEX_API_URL = 'https://ws-api.iextrading.com/1.0';
+const IEX_API_URL = 'https://api.iextrading.com/1.0';
 
-module.exports = {port};
+module.exports = {port, WS_IEX_API_URL, IEX_API_URL};
--- server/engine/Engine.js
+++ server/engine/Engine.js
@@ -1,12 +1,31 @@
 const WebSocket = require('ws');
+const {WS_IEX_API_URL} = require('../config');
 const millisecondsInAnHour = 360000000000;
 
 class Engine {
     constructor() {
         this.interval = null;
-        this.websocketClient = new WebSocket(`ws://${host}:${port}`);
-        this.websocketClient.on('message', this.handleWebSocketMessage);
+        this.wsClient = new WebSocket(WS_IEX_API_URL);
+
         this.analyze = this.analyze.bind(this);
+        this.handleWebSocketConnect = this.handleWebSocketConnect.bind(this);
+        this.handleWebSocketMessage = this.handleWebSocketMessage.bind(this);
+
+        this.registerEvents();
+    }
+
+    registerEvents() {
+        this.wsClient.on('connect', this.handleWebSocketConnect);
+        this.wsClient.on('message', this.handleWebSocketMessage);
+    }
+
+    handleWebSocketConnect() {
+        // Subscribe to topics (i.e. appl,fb,aig+)
+        this.wsClient.emit('subscribe', 'snap,fb,aig+');
+    }
+
+    handleWebSocketMessage(msg) {
+        console.log(msg);
     }
 
     start() {
